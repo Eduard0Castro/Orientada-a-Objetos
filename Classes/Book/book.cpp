e2579787901635efb06972fda11a98d1f766d41c
Eduardo Castro <americogomes1@outlook.com>
2023-09-25 13:11:42 -0300

    Book class; Why use hierarchy?

---
+++ Classes/Book/book.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "book.h"
+
+using namespace std;
+
+Book::Book(){
+    title = "Unknown";
+    author = "Unknown";
+    company = "Unknown";
+    genre = "Unknown";
+}
+
+Book::Book(string t, string a, string c, string g){
+    title = t;
+    author = a;
+    company = c;
+    genre = g;
+}
+
+string Book::getTitle(){
+    return title;
+}
+string Book::getAuthor(){
+    return author;
+}
+string Book::getCompany(){
+    return company;
+}
+string Book::getGenre(){
+    return genre;
+}
+
+void Book::setTitle(string t){
+    title = t;
+}
+void Book::setAuthor(string a){
+    author = a;
+}
+void Book::setCompany(string c){
+    company = c;
+}
+void Book::setGenre(string g){
+    genre = g;
+}
+
+/*Para implementar essa classe para uma livraria, 
+seria necessário incluir a variável preço aos membros privados, 
+o que se adequa ao uso de classes herdeiras. */
+
---
+++ Classes/Book/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include"book.h"
+
+int main(){
+
+    Book galgo("As Ilhas da Corrente", "Ernest Hemingway", "Bertrand Brasil", "Romance");
+    Book unknown;
+
+    cout << galgo;
+    cout << unknown;
+
+
+}
